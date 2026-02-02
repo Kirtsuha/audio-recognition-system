0c946e75e87a42463104f59758a8d2b3f043ab75
Kirtsuha <batukhtin.kir@gmail.com>
2026-02-02 18:51:25 +0300

    added database logic

---
+++ repository/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, Text, ForeignKey, BigInteger
+
+from repository.database import Base
+
+
+class Track(Base):
+    __tablename__ = "track"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, nullable=False)
+    artist = Column(Text, nullable=False)
+
+class Fingerprint(Base):
+    __tablename__ = "fingerprint"
+
+    hash = Column(BigInteger, primary_key=True)
+    track_id = Column(Integer, ForeignKey("track.id"), nullable=False)
+    time_offset = Column(Integer, nullable=False)
