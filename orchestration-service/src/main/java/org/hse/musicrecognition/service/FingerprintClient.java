9b2404fb8a5826d30eee22e4e2de5a5f5851bc0e
Kirtsuha <batukhtin.kir@gmail.com>
2026-02-09 01:34:54 +0300

    fixed client service

--- orchestration-service/src/main/java/org/hse/musicrecognition/service/FingerprintClient.java
+++ orchestration-service/src/main/java/org/hse/musicrecognition/service/FingerprintClient.java
@@ -4,8 +4,11 @@ package org.hse.musicrecognition.service;
 import lombok.RequiredArgsConstructor;
 import org.hse.musicrecognition.dto.FingerprintResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -19,10 +22,15 @@ public class FingerprintClient {
     private String fingerprintUrl;
 
     public Mono<FingerprintResponse> recognize(byte[] audioBytes) {
+        MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
+        bodyBuilder.part("file", audioBytes)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE)
+                .header("Content-Disposition", "form-data; name=\"file\"; filename=\"audio.wav\"");
+
         return webClient.post()
                 .uri(fingerprintUrl)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .bodyValue(audioBytes)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
                 .retrieve()
                 .bodyToMono(FingerprintResponse.class);
     }
